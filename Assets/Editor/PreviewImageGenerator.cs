75e182bb2c52df959416597cd363b724033a1c4d
ambid17 <ambid17@gmail.com>
2025-08-13 01:02:47 -0500

    Asset generation working

--- Assets/Editor/PreviewImageGenerator.cs
+++ Assets/Editor/PreviewImageGenerator.cs
@@ -17,12 +17,23 @@ public class PreviewImageGenerator : EditorWindow
         GUILayout.Label("This creates a preview image");
         if (GUILayout.Button("Create"))
         {
-            GetPreviews(Selection.activeObject as GameObject);
+            GeneratePrefabPreview(Selection.activeObject as GameObject);
         }
     }
 
-    private void GetPreviews(GameObject prefab)
+    public static void GeneratePrefabPreview(GameObject prefab)
     {
+        string texturesFolder = Path.Combine(Application.dataPath, "Textures");
+        string previewsFolder = Path.Combine(texturesFolder, "GeneratedPreviews");
+        string fileName = $"{prefab.name}.png";
+        string fullPath = Path.Combine(previewsFolder, fileName);
+        if(File.Exists(fullPath))
+        {
+            Debug.Log($"Preview already exists for {prefab.name} at {fullPath}");
+            return;
+        }
+
+        Debug.Log($"Generating preview for: {prefab.name}");
         AssetPreview.SetPreviewTextureCacheSize(2);
 
         Texture2D preview = null;
@@ -36,14 +47,11 @@ public class PreviewImageGenerator : EditorWindow
         {
             preview.Apply();
             byte[] data = preview.EncodeToPNG();
-            string texturesFolder = Path.Combine(Application.dataPath, "Textures");
-            string previewsFolder = Path.Combine(texturesFolder, "GeneratedPreviews");
-            string fileName = $"{prefab.name}.png";
-            string fullPath = Path.Combine(previewsFolder, fileName);
 
             if (!File.Exists(fullPath))
             {
                 File.WriteAllBytes(fullPath, data);
+                AssetDatabase.Refresh();
             }
         }
     }
---
+++ Assets/Editor/ScriptableObjectGenerator.cs
@@ -0,0 +1,129 @@
+using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ScriptableObjectGenerator : EditorWindow
+{
+    [SerializeField] private List<GameObject> prefabs;
+    [SerializeField] private int cost;
+    [SerializeField] private string costInput;
+    private SerializedObject serializedObject;
+
+    [MenuItem("Tools/ScriptableObjectGenerator")]
+    private static void GeneratePreview()
+    {
+        GetWindow<ScriptableObjectGenerator>();
+    }
+
+    private void OnGUI()
+    {
+        ScriptableObject scriptableObj = this;
+        SerializedObject serialObj = new SerializedObject(scriptableObj);
+        SerializedProperty serialProp = serialObj.FindProperty("prefabs");
+
+        EditorGUILayout.PropertyField(serialProp, true);
+        serialObj.ApplyModifiedProperties();
+
+
+        costInput = EditorGUILayout.TextField("Cost", costInput);
+        if (int.TryParse(costInput, out int parsedCost))
+        {
+            cost = parsedCost;
+        }
+
+        if (GUILayout.Button("Create"))
+        {
+            foreach (var prefab in prefabs)
+            {
+                Debug.Log($"Generating ScriptableObject for {prefab.name} with cost {cost}");
+                GenerateSo(prefab, cost);
+            }
+        }
+    }
+
+    private void GenerateSo(GameObject prefab, int cost)
+    {
+        PreviewImageGenerator.GeneratePrefabPreview(prefab);
+
+        string scriptableObjectFolder = "Assets/ScriptableObjects/ShopItems/Items";
+        var existingScriptableObjects = AssetDatabase.FindAssets($"{prefab.name}", new string[] { scriptableObjectFolder });
+        if (existingScriptableObjects != null && existingScriptableObjects.Length > 0)
+        {
+            Debug.LogWarning($"ScriptableObject already exists for {prefab.name} in {scriptableObjectFolder}. Skipping creation.");
+            return;
+        }
+
+        var itemIcon = GetSpriteFromAssets(prefab);
+
+        if (itemIcon == null)
+        {
+            Debug.LogWarning($"No sprite found for {prefab.name}.");
+            return;
+        }
+        var scriptableObject = CreateInstance<SortableItem>();
+        scriptableObject.cost = cost;
+        scriptableObject.name = prefab.name;
+        scriptableObject.itemName = prefab.name;
+        scriptableObject.icon = itemIcon;
+
+        AssetDatabase.CreateAsset(scriptableObject, $"{scriptableObjectFolder}/{prefab.name}.asset");
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    private Sprite GetSpriteFromAssets(GameObject prefab)
+    {
+        string[] previewsFolder = new string[] { "Assets/Textures/GeneratedPreviews" };
+        var assets = AssetDatabase.FindAssets($"{prefab.name}", previewsFolder);
+
+        if(assets == null || assets.Length == 0)
+        {
+            Debug.LogWarning($"No assets found for {prefab.name} in {previewsFolder}");
+            return null;
+        }
+
+        var assetPath = AssetDatabase.GUIDToAssetPath(assets[0]);
+        Debug.Log($"Found asset: {assets[0]} at path: {assetPath}");
+
+        var sprite = AssetDatabase.LoadAssetAtPath<Sprite>(assetPath);
+        if (sprite != null)
+        {
+            Debug.Log($"Loaded sprite for {prefab.name} at {assetPath}");
+            return sprite;
+        }
+        else
+        {
+            Debug.LogWarning($"Sprite not found for {prefab.name} at {assetPath}. Trying to find a texture, and convert to a sprite");
+            TrySpriteReformat(assetPath);
+        }
+
+        return null;
+    }
+
+    private Sprite TrySpriteReformat(string assetPath)
+    {
+        var texture = AssetDatabase.LoadAssetAtPath<Texture>(assetPath);
+
+        Debug.Log($"Reformatting texture at {assetPath} to sprite");
+        TextureImporter textureImporter = AssetImporter.GetAtPath(assetPath) as TextureImporter;
+        textureImporter.textureType = TextureImporterType.Sprite;
+        textureImporter.spriteImportMode = SpriteImportMode.Single;
+        textureImporter.SaveAndReimport();
+
+        var sprite = AssetDatabase.LoadAssetAtPath<Sprite>(assetPath);
+        if(sprite == null)
+        {
+            Debug.LogError($"Failed to convert texture at {assetPath} to sprite.");
+            return null;
+        }
+
+        Debug.Log($"Reimported texture at {assetPath} to sprite");
+        return sprite;
+    }
+
+}
