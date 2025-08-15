e772d4383ba2fda35c1cf6a9a46461990bf1dfc4
ambid17 <ambid17@gmail.com>
2025-08-14 23:27:40 -0500

    Add build version tool

---
+++ Assets/Editor/BuildIncrementer.cs
@@ -0,0 +1,13 @@
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class BuildIncrementer : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 1;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        throw new System.NotImplementedException();
+    }
+}
